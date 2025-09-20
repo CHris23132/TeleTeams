7f9d15376c612d9233bd7f9f8bb003232183a3e7
CHris23132 <play3dinc@gmail.com>
2025-09-20 13:56:12 -0400

    Initial Commit

---
+++ Tele Teams/Tele_TeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tele_TeamsApp.swift
+//  Tele Teams
+//
+//  Created by Chris on 2025-09-20.
+//
+
+import SwiftUI
+
+@main
+struct Tele_TeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
